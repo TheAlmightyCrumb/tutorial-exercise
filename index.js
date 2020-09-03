6660f20f3c00a42fb9a35613e74343296a0a4810
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-09-03 16:58:08 +0300

    Creating first methods
    Creating 2 'get' and 1 'post' requests for my api.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const Tutorial = require('./models/tutorial');
 
@@ -5,3 +6,36 @@ const app = express();
 
 app.use(express.json());
 
+/* Get all tutorials */
+app.get('/api/tutorials', (req, res) => {
+    Tutorial.find({}).then(tutorial => {
+      res.json(tutorial);
+    })
+  });
+
+/* Get tutorial by id */
+app.get('/api/tutorials/:id', (req, res) => {
+    Tutorial.findById(req.params.id)
+    .then(tutorial => tutorial ? res.json(tutorial) : res.status(404).end())
+    .catch(err => {
+        console.log(err);
+        res.status(400).send({error: 'Malformatted Id...'});
+    })
+});
+
+/* Add new tutorial */
+app.post('/api/tutorials', (req, res) => {
+    const { name = undefined, published = false } = req.body;
+    const tutorial = new Tutorial({
+        name,
+        published
+    });
+    tutorial.save().then(newTutorial => {
+        res.json(newTutorial);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+})
